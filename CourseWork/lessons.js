5f99ce09992671cb73697101522611b1e6de54d8
serleh <56731547+serleh@users.noreply.github.com>
2021-04-04 13:57:01 +0100

    Add files via upload

---
+++ CourseWork/lessons.js
@@ -0,0 +1,100 @@
+let myArr = [
+    {
+      "id": 1001,
+      "title": "ARABIC LANGUAGE ",
+      "description": "your knowledge of arabic.",
+      "location":"New Lectures room",
+      "price": 20.00,
+      "image": "images/arabic.jpg",
+      "availableInventory": 10
+     
+    },
+    {
+      "id": 1002,
+      "title": "HAUSA",
+      "description": " with Nigerians based in Dubai.",
+      "location":"Room 444",
+      "price": 2.99,
+      "image": "images/hausa.jpg",
+      "availableInventory": 7,
+    
+    },
+    {
+      "id": 1003,
+      "title": "Javascript for Babies",
+      "description": " the programming world.",
+      "location":"Site Campus",
+      "price": 11.00,
+      "image": "images/javaBabies.jpg",
+      "availableInventory": 99,
+      
+    },
+    {
+      "id": 1004,
+      "title": "Machine learning",
+      "description": " at your free time",
+      "location":"Dubai Marina",
+      "price": 7.99,
+      "image": "images/ml.jpg",
+      "availableInventory": 11,
+     
+    },
+    {
+      "id": 1005,
+      "title": "Google Adword",
+      "description": " your google campaign",
+      "location":"Sharja",
+      "price": 49.99,
+      "image": "images/ga.jpg",
+      "availableInventory": 25,
+     
+    },
+    {
+      "id": 1006,
+      "title": "Business Intelligence",
+      "description": "world using technology.",
+      "location":"Internet City",
+      "price": 45.99,
+      "image": "images/bi.webp",
+      "availableInventory": 25,
+    },
+    {
+      "id": 1007,
+      "title": "Maths for nerds",
+      "description": " solve logical equation",
+      "location":"Myriad",
+      "price": 49.99,
+      "image": "images/mas.jpg",
+      "availableInventory": 25,
+    },
+    {
+      "id": 1008,
+      "title": "French language",
+      "description": "Bonjour!! learn french with mw",
+      "location":"Bostom",
+      "price": 9.99,
+      "image": "images/fr.jpg",
+      "availableInventory": 5,
+    },
+    {
+      "id": 1009,
+      "title": "React js",
+      "description": " Create React redux in simple form",
+      "location":"BUK",
+      "price": 41.09,
+      "image": "images/rct.png",
+      "availableInventory": 15,
+    },
+    {
+      "id": 1010,
+      "title": "C++",
+      "description": "Data Mining with C++",
+      "location":"Room 406",
+      "price": 11.11,
+      "image": "images/dm.png",
+      "availableInventory": 25,
+    },
+    
+
+
+  ]
